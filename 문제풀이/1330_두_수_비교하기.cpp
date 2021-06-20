763d0c2dbfe80c13d7b73b73bd28bb40f842f818
mathpaul3 <mathpaul3@gmail.com>
2021-06-20 22:11:44 +0900

    modify

--- 문제풀이/1330_두_수_비교하기.cpp
+++
@@ -1,7 +0,0 @@
-#include <stdio.h>
-int main() {
-	 int a, b;
-	 scanf("%d %d", &a, &b);
-	 
-	 a>b ? printf(">") : a==b ? printf("==") : printf("<");
-}
--- 미해결/1010.c
+++ 미해결/1010.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 
-int main() {
+int main()
+{
 	int T, N, M;
 	scanf("%d", &T);
-	while(--T) {
+	while (--T)
+	{
 		scanf("%d %d", &N, &M);
-		mCn	
 	}
-	
 }
