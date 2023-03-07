b790b32a7647267de8a5b947468d27c819fb5bb6
mathpaul3 <mathpaul3@gmail.com>
2023-03-07 23:18:04 +0900

    골드바흐의 추측

--- baekjoon/cpp/6588.cpp
+++ baekjoon/cpp/6588.cpp
@@ -1,57 +1,43 @@
 #include <iostream>
-#include <vector>
+#include <bitset>
+
 using namespace std;
 
-vector<int> prime;
+const int MAX = 1e6;
 
-void init() {
-    for (int i=2; i<1001000; i++) {
-        int sz = prime.size();
-        bool is_prime = true;
-        for (int j=0; j<sz; j++) {
-            if (!(i % prime[j])) {
-                is_prime = false;
-                break;
+bitset<MAX> is_prime; // a bitset to store prime numbers
+
+void sieve() {
+    is_prime.set();
+    is_prime[0] = is_prime[1] = 0;
+
+    for (int i = 2; i * i < MAX; i++) {
+        if (is_prime[i]) {
+            for (int j = i * i; j < MAX; j += i) {
+                is_prime[j] = 0;
             }
-            if (prime[j]*prime[j] >= i)
-                break;
         }
-        if (is_prime)
-            prime.push_back(i);
     }
 }
 
 int main() {
     ios_base::sync_with_stdio(false);
     cin.tie(NULL); cout.tie(NULL);
+    sieve();
 
-    init();
-
-    int n=1;
-    while(n) {
-        cin >> n;
-        if (!n)
-            break;
-
-        int M = 0;
-        while (prime[++M] < n)
-            true;
-        
-        M--;
+    int n;
+    while (cin >> n && n != 0) {
         bool found = false;
-        for (int i=1; prime[i]<n; i++) {
-            if (found)
+        for (int i = 3; i <= n / 2; i += 2) {
+            if (is_prime[i] && is_prime[n - i]) {
+                cout << n << " = " << i << " + " << n - i << '\n';
+                found = true;
                 break;
-            for (int j=M; i<=j; j--) {
-                if ((prime[i] + prime[j]) == n) {
-                    cout << n << " = " << prime[i] << " + " << prime[j] << '\n';
-                    found = true;
-                    break;
-                }
             }
         }
-        if (!found)
-            cout << "Goldbach's conjecture is wrong.\n";
+        if (!found) {
+            cout << "Goldbach's conjecture is wrong." << '\n';
+        }
     }
 
     return 0;
